c6170a056c05b4fc921ac212e37785c37455f42b
violetflare@protonmail.com <violetflare@protonmail.com>
2022-12-14 21:14:58 +0100

    fix

---
+++ src/Instance.js
@@ -0,0 +1,82 @@
+const Heartbeater = require("./Heartbeater.js");
+
+const Controller = require("./Controller.js");
+const ResponseHandler = require("./ResponseHandler.js");
+const Requester = require("./Requester.js");
+
+class Instance {
+
+    constructor(wss, ws, cache) {
+        this.wss = wss;
+        this.ws = ws;
+        this.cache = cache;
+
+        this.controller = new Controller(this.cache);
+        this.responseHandler = new ResponseHandler(this.cache);
+        this.requester = new Requester(this.cache);
+    }
+
+    _listenForRequests() {
+        this.ws.on("message", (data) => {
+            const jsonString = data.toString("utf8");
+            const json = JSON.parse(jsonString);
+
+            const isEmpty = Object.keys(json).length === 0;
+
+            if (!isEmpty) {
+                const isRequest = json.route;
+
+                if (isRequest) {
+                    const route = json.route;
+                    const data = json.data;
+                    const response = this.controller.callRoute(route, data);
+                    this.ws.send(response);
+                }
+            }
+        });
+    }
+
+    _sendRequest(route, data = {}) {
+        const response = new Promise((resolve, reject) => {
+            const _handler = (data) => {
+                const jsonString = data.toString("utf8");
+                const response = JSON.parse(jsonString);
+    
+                const isEmpty = Object.keys(response).length === 0;
+    
+                if (!isEmpty) {
+                    const isResponse = response.calledRoute && route === response.calledRoute;
+    
+                    if (isResponse) {
+                        this.ws.removeEventListener("message", _handler);
+
+                        resolve(response);
+                    }
+                }
+            };
+
+            this.ws.on("message", _handler);
+
+            const request = this.requester.createRequest(route, data);
+
+            this.ws.send(JSON.stringify(
+                request
+            ));
+        });
+
+        return response;
+    }
+
+    init() {
+        new Heartbeater(this.wss, this.ws).init();
+
+        this._listenForRequests();
+
+        this._sendRequest('/guilds').then(response => console.log(response));
+        this._sendRequest('/guilds').then(response => console.log(response));
+        this._sendRequest('/guilds').then(response => console.log(response));
+    }
+
+}
+
+module.exports = Instance;
--- src/InstanceManager.js
+++ src/InstanceManager.js
@@ -13,83 +13,19 @@ const Cache = require("./Cache.js");
 const WebSocketServer = require("ws").WebSocketServer;
 const config = require("../config.js");
 
-const Heartbeater = require("./Heartbeater.js");
+const Instance = require('./Instance.js');
 
-const Controller = require("./Controller.js");
-const ResponseHandler = require("./ResponseHandler.js");
-const Requester = require("./Requester.js");
-
-class Instance {
+class InstanceManager {
 
     constructor() {
-        this.controller = new Controller(Cache);
-        this.responseHandler = new ResponseHandler(Cache);
-        this.requester = new Requester(Cache);
-    }
-
-    _listenForRequests() {
-        this.ws.on("message", (data) => {
-            const jsonString = data.toString("utf8");
-            const json = JSON.parse(jsonString);
-
-            const isEmpty = Object.keys(json).length === 0;
-
-            if (!isEmpty) {
-                const isRequest = json.route;
-
-                if (isRequest) {
-                    const route = json.route;
-                    const data = json.data;
-                    const response = this.controller.callRoute(route, data);
-                    this.ws.send(response);
-                }
-            }
-        });
-    }
-
-    _sendRequest(route, data = {}) {
-        const response = new Promise((resolve, reject) => {
-            const _handler = (data) => {
-                const jsonString = data.toString("utf8");
-                const response = JSON.parse(jsonString);
-    
-                const isEmpty = Object.keys(response).length === 0;
-    
-                if (!isEmpty) {
-                    const isResponse = response.calledRoute && route === response.calledRoute;
-    
-                    if (isResponse) {
-                        this.ws.removeEventListener("message", _handler);
-
-                        resolve(response);
-                    }
-                }
-            };
-
-            this.ws.on("message", _handler);
-
-            const request = this.requester.createRequest(route, data);
-
-            this.ws.send(JSON.stringify(
-                request
-            ));
-        });
-
-        return response;
+        this.sessions = [];
     }
 
     _setEvents() {
         this.wss.on("connection", (ws) => {
-            this.ws = ws;
-
-            new Heartbeater(this.wss, this.ws).init();
-
-            this._listenForRequests();
-
-            this._sendRequest('/guilds').then(response => console.log(response));
-            this._sendRequest('/guilds').then(response => console.log(response));
-            this._sendRequest('/guilds').then(response => console.log(response));
-            
+            const instance = new Instance(this.wss, ws, Cache);
+            instance.init();
+            this.sessions.push
         });
     }
 
@@ -124,4 +60,4 @@ class Instance {
     }
 }
 
-module.exports = Instance;
+module.exports = InstanceManager;
